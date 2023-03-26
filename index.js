4c10ebe3bc8ceaea6252ce31ab45c633c6e9be35
+ <+>
2023-03-26 16:51:01 -0400

    functions cont

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(doSomething = "go to the office"){
+    return `This Monday, I will ${doSomething}.`;
+}
+mondayWork();
+mondayWork("go to the office")
+
+function wrapAdjective(rando = "*"){
+    return function (character = "special"){
+        return `You are ${rando}${character}${rando}!`
+    }
+}
+wrapAdjective("*a hard worker*");
+wrapAdjective("||a dedicated programmer||")
